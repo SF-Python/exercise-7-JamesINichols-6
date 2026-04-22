02fcd4c582dbddab26524ab718e0be79d94b3c01
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2026-04-22 09:49:14 -0400

    Alex L

--- exercise7.py
+++ exercise7.py
@@ -1,41 +1,62 @@
-# Kailey Swarthout
-# COP2002.0M1
-# 3/28/2026
-# Exercise 7: Define Own Function (Generate MAC Address)
+# Alex Libertowski
+# COP2002-0M1
+# April 01, 2026
+# Exercise 7: Define Own Functions and Use It
+# Practicing the use of functions by generating MAC and IPv6 addresses
 
-import random
+from random import choice
 
-def generate_hex_digits(number = 6):
-    hex_digits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-    pairs = []
-    count = 0
 
-# Hex Pairs
-    while count < number:
-        first = random.choice(hex_digits)
-        second = random.choice(hex_digits)
-        pairs.append(first + second)
-        count += 1
+def generate_hex_digits(number=6):
+    hexValues = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+    ]
 
-# MAC address
+    HEX_CHUNKS = 2
+    COLON_INTERVAL = 2
     if number == 6:
-        return ":".join(pairs)
-    
-    # IPv6: 2 pairs -> quartets
-    if number == 16:
-        quartets = []
-        i = 0
-        while i < len(pairs):
-            quartets.append(pairs[i] + pairs[i+1])
-            i += 2
-        return ":".join(quartets)
-        
-def main():
-    mac = generate_hex_digits()
-    ipv6 = generate_hex_digits(16)
+        COLON_INTERVAL = 2
+    else:
+        COLON_INTERVAL = 4
 
-    print(f"Generating random MAC address.... {mac}.")
-    print(f"Generating random ipv6 address.... {ipv6}.")
+    address = ""
+    i = 0
+    for i in range(number * HEX_CHUNKS):
+        # Add a colon at every two hex values, except the first index
+        if i != 0 and i % COLON_INTERVAL == 0:
+            address += ":"
+
+        hexValue = choice(hexValues)
+        address += hexValue
+        i += 1
+
+    return address
+
+
+def main():
+    print("Generating random MAC address...")
+    hexAddress = generate_hex_digits()
+    print(hexAddress)
+
+    print("\nGenerating random IPv6 address...")
+    hexAddress = generate_hex_digits(16)
+    print(hexAddress)
+
 
 if __name__ == "__main__":
     main()
