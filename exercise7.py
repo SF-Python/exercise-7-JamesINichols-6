75485e535add30e8dd34320883728e54adebec14
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2026-04-24 08:39:53 -0400

    Allison

--- exercise7.py
+++ exercise7.py
@@ -1,49 +1,60 @@
-# Elijah Morris
-# COP2002 0M1
-# 4/11/2026
-# Random Hexadecimal Generator for MAC and IPv6 Addresses
-# This program generates random hexadecimal digits to create valid MAC addresses and IPv6 addresses.
-
-import random 
-
-def generate_hex_digits(number=6):
-    hex_digits = "0123456789ABCDEF"
-    result = ""
-
-    # MAC address 
-    if number == 6:
-        for i in range(number):
-            pair = ""
-            for j in range(2):
-                pair += random.choice(hex_digits)
-            result += pair
-            if i < number - 1:
-                result += ":"
-
-    # IPv6 address 
-    elif number == 16:
-        for i in range(8):  # 8 quartets
-            quartet = ""
-            for j in range(4):
-                quartet += random.choice(hex_digits)
-            result += quartet
-            if i < 7:
-                result += ":"
-
-    return result
-
+# Allison Day
+# COP2002.0M1
+# April 5, 2026
+# Exercise 7
+# This program will generate a random MAC address or IPv6 address
 
 def main():
-    print("Generating random MAC address...")
-    mac = generate_hex_digits()
-    print(mac)
+    import random
 
+    choices = [6, 16]
+    number = random.choice(choices)
+
+
+def generate_hex_digits(number=6):
+    print("Generating random MAC Address...")
+    
+    # Uses random module to generate a random hex digit
+    import random
+
+    # Lists all possible hex digits that can be used
+    hex_digits = "0123456789ABCDEF"
+    mac_address = ""
+    
+    # Loop for 6 pairs to complete the MAC Address
+    for int in range(6):
+        pair = random.choice(hex_digits) + random.choice(hex_digits)
+        mac_address += pair
+        if int < 5:
+
+            # Adds colon between pairs
+            mac_address += ":"
+    print(mac_address)
+    print() 
+    
+def generate_hex_digits(number=16):
+    print("Generating random IPv6 Address...")
+    
+    # Uses random module to generate a random hex digit
+    import random
+
+    # Lists all possible hex digits that can be used
+    hex_digits = "0123456789ABCDEF"
+    ipv6_address = ""
+    
+    # Loop for 8 pairs to complete the IPv6 Address
+    for int in range(8):
+        pair = random.choice(hex_digits) + random.choice(hex_digits)
+        ipv6_address += pair
+        if int < 7:
+
+            # Adds colon between pairs
+            ipv6_address += ":"
+    print(ipv6_address)
     print()
 
-    print("Generating random IPv6 address...")
-    ipv6 = generate_hex_digits(16)
-    print(ipv6)
+if (main() == generate_hex_digits(number=6)):
+    main()
 
-
-if(__name__=="__main__"):
+elif (main() == generate_hex_digits(number=16)):
     main()
